ab096d39eced204d2a2da7e713961898194bf21b
Renato-Rodrigues <renato.rodrigues@pik-potsdam.de>
2025-08-07 19:44:14 +0200

    release candidate for efefctive carbon price function and subtypes

--- R/convertWBCarbonPricingDashboard.R
+++ R/convertWBCarbonPricingDashboard.R
@@ -3,44 +3,22 @@
 #' @md
 #' @param x A [`magpie`][magclass::magclass] object returned from
 #'          [`readWBCarbonPricingDashboard()`].
-#' 
-#' @param subtype character, type of data: price, price_1_april, coverage, revenue, emissions_share or emissions
-#' 
+#'
+#' @param subtype character, type of data: price, priceAprilFirst, wbCoverage, revenue, emissions_covered
+#'
 #' @return A [`magpie`][magclass::magclass] object.
 #'
 #' @author Renato Rodrigues
-#' 
-#' @importFrom madrat calcOutput toolCountryFilln toolFillYears
+#'
+#' @importFrom madrat calcOutput toolCountryFill toolFillYears
 #' @importFrom magclass dimSums getYears<- getYears mbind
 #' @export
 #'
 convertWBCarbonPricingDashboard <- function(x, subtype) {
 
-  if(subtype %in% c("emissions_share", "emissions")) {
-    #correct emissions share data disaggregation for EU_ETS countries
-    EU27 <- c("Austria" = "AUT", "Belgium" = "BEL", "Bulgaria" = "BGR", "Croatia" = "HRV", "Cyprus" = "CYP", "Czech Republic" = "CZE", "Denmark" = "DNK", "Estonia" = "EST", "Finland" = "FIN", "France" = "FRA", "Germany" = "DEU", "Greece" = "GRC", "Hungary" = "HUN", "Ireland" = "IRL", "Italy" = "ITA", "Latvia" = "LVA", "Lithuania" = "LTU", "Luxembourg" = "LUX", "Malta" = "MLT", "Netherlands" = "NLD", "Poland" = "POL", "Portugal" = "PRT", "Romania" = "ROU", "Slovakia" = "SVK", "Slovenia" = "SVN", "Spain" = "ESP", "Sweden" = "SWE")
-    EU_ETS <- c(EU27, "Iceland" =	"ISL", "Liechtenstein" = "LIE", "Norway" = "NOR")
-
-    emi.raw <- magclass::dimSums(madrat::readSource("EDGARghg"),3, na.rm = TRUE)
-
-    #copying last year for missing years
-    tmp <- tmp2 <- emi.raw[,2023,]
-    magclass::getYears(tmp) <- 2024
-    magclass::getYears(tmp2) <- 2025
-    emi <- magclass::mbind(emi.raw,tmp,tmp2)[,magclass::getYears(x),]
-
-    s <- emi[EU_ETS,,] / sum(emi[EU_ETS,,], na.rm = TRUE)
-    x[EU_ETS,,] <- s[EU_ETS,,]*x[EU_ETS,,]
-
-  }
-
-  if (subtype == "emissions") {
-    x <- x * magclass::dimSums(emi,1)
-  }
-
   x[is.na(x)] <- 0 # setting NA values to zero
-  x <- x[,sort(magclass::getYears(x, as.integer = T)),]
-  x <- madrat::toolFillYears(x, sort(magclass::getYears(x, as.integer = T)))
+  x <- x[, sort(magclass::getYears(x, as.integer = TRUE)), ]
+  x <- madrat::toolFillYears(x, sort(magclass::getYears(x, as.integer = TRUE)))
   x <- madrat::toolCountryFill(x, fill = 0, verbosity = 2)
 
   return(x)
--- R/readWBCarbonPricingDashboard.R
+++ R/readWBCarbonPricingDashboard.R
@@ -2,79 +2,77 @@
 #'
 #' This data can be downloaded directly from the site.
 #'
-#' @param subtype character, type of data: price, price_1_april, coverage, revenue, emissions_share or emissions
+#' @param subtype character, type of data: price, priceAprilFirst, wbCoverage, revenue, emissions_covered
 #' @returns MagPIE object with world banck data on carbon price per country
 #'
 #' @author Renato Rodrigues
 #'
 #' @importFrom readxl read_excel
-#' @importFrom dplyr select all_of matches mutate left_join across filter %>% rename_with join_by 
-#' bind_rows case_when .data
+#' @importFrom dplyr select all_of matches mutate left_join across filter %>% rename_with join_by group_by
+#' bind_rows case_when .data summarize coalesce where
 #' @importFrom tidyr extract pivot_longer
 #' @importFrom magclass as.magpie
 #' @importFrom madrat toolCountryFill toolGetMapping
+#' @importFrom quitte as.quitte
 #' @export
-#' 
+#'
 readWBCarbonPricingDashboard <- function(subtype = "price") {
 
   dashboardFile <- "data_2025.xlsx"
 
-  raw.info <- suppressMessages(readxl::read_excel(dashboardFile, sheet = "Compliance_Gen Info", skip = 1, .name_repair = "unique")) %>%
+  rawInfo <- suppressMessages(
+    readxl::read_excel(dashboardFile, sheet = "Compliance_Gen Info", skip = 1, .name_repair = "unique")
+  ) %>%
     rename_with(~gsub(" ", "_", tolower(.x)))
-  raw.emissions <- readxl::read_excel(dashboardFile, sheet = "Compliance_Emissions", skip = 2) %>%
+  rawEmissions <- readxl::read_excel(dashboardFile, sheet = "Compliance_Emissions", skip = 2) %>%
     rename_with(~gsub(" ", "_", tolower(.x)))
-  raw.revenue <- readxl::read_excel(dashboardFile, sheet = "Compliance_Revenue", skip = 1) %>%
+  rawRevenue <- readxl::read_excel(dashboardFile, sheet = "Compliance_Revenue", skip = 1) %>%
     rename_with(~gsub(" ", "_", tolower(.x)))
-  raw.price <- readxl::read_excel(dashboardFile, sheet = "Compliance_Price", skip = 1) %>%
+  rawPrice <- readxl::read_excel(dashboardFile, sheet = "Compliance_Price", skip = 1) %>%
     rename_with(~gsub(" ", "_", tolower(.x)))
 
-  wb.regionMapping <- toolGetMapping("wbRegion.csv", type = "regional", where = "mrPEM")
+  wbRegionMapping <- toolGetMapping("wbRegion.csv", type = "regional", where = "mrPEM")
+  wbSectoralMapping <- toolGetMapping("wbSector.csv", type = "sectoral", where = "mrPEM")
 
-  wb.sectoralMapping <- toolGetMapping("wbSector.csv", type = "sectoral", where = "mrPEM")
+  metadata <- rawInfo  %>%
+    left_join(wbRegionMapping, by = "unique_id") %>%
+    left_join(wbSectoralMapping, by = "unique_id") %>%
+    dplyr::select(c("unique_id", "region", "region_type", "instrument_name", "type", "status", "jurisdiction_covered",
+                    "gases_covered", "sector_group", "electricity_and_heat", "industry",
+                    "mining_and_extractives", "transport", "aviation", "buildings",
+                    "agriculture,_forestry_and_fishing_fuel_use", "agricultural_emissions", "waste", "lulucf",
+                    "fuels_covered", "allocation_approaches", "price_or_market_management", "point_of_regulation",
+                    "offset_eligibility", "description", "recent_developments", "coverage", "pricing_and_allocation",
+                    "compliance", "relation_to_other_instruments")) %>%
+    dplyr::left_join(rawPrice %>% dplyr::select(c("unique_id", "income_group")), by = "unique_id") %>% # nolint
+    dplyr::mutate(type = case_when(type == "Carbon tax" ~ "carbon_tax",
+                                   type == "ETS"        ~ "ets",
+                                   type == "Undecided"  ~ NA,
+                                   type == "Carbon Tax" ~ "carbon_tax"),
+                  status = case_when(status == "Implemented"         ~ "implemented",
+                                     status == "Under consideration" ~ "under_consideration",
+                                     status == "Abolished"           ~ "abolished",
+                                     status == "Under development"   ~ "under_development"))
 
-  d.metadata <- raw.info  %>%
-    left_join(wb.regionMapping, by = "unique_id") %>%
-    left_join(wb.sectoralMapping, by = "unique_id") %>%
-    dplyr::select(
-      c("unique_id", "region", "region_type", "instrument_name", "type", "status", "jurisdiction_covered", "gases_covered", "sector_group", "electricity_and_heat", "industry",
-        "mining_and_extractives", "transport", "aviation", "buildings", "agriculture,_forestry_and_fishing_fuel_use",
-        "agricultural_emissions", "waste", "lulucf", "fuels_covered", "allocation_approaches", "price_or_market_management",
-        "point_of_regulation", "offset_eligibility", "description", "recent_developments", "coverage", "pricing_and_allocation", "compliance", "relation_to_other_instruments")) %>%
-    dplyr::left_join(
-      raw.price %>%
-        dplyr::select(c("unique_id", "income_group"))
-      , by = "unique_id") %>%
-    dplyr::mutate(
-      type = case_when(
-        type == "Carbon tax" ~ "carbon_tax",
-        type == "ETS"        ~ "ets",
-        type == "Undecided"  ~ NA,
-        type == "Carbon Tax" ~ "carbon_tax"),
-      status = case_when(
-        status == "Implemented"         ~ "implemented",
-        status == "Under consideration" ~ "under_consideration",
-        status == "Abolished"           ~ "abolished",
-        status == "Under development"   ~ "under_development")
-    )
-
-  d.coverage <- raw.info  %>%
-    left_join(wb.regionMapping, by = "unique_id") %>%
-    left_join(wb.sectoralMapping, by = "unique_id") %>%
-    dplyr::select(c("unique_id", "region", "region_type", "sector_group", "share_of_jurisdiction_emissions_covered")) %>%
+  wbCoverage <- rawInfo  %>%
+    left_join(wbRegionMapping, by = "unique_id") %>%
+    left_join(wbSectoralMapping, by = "unique_id") %>%
+    dplyr::select(c("unique_id", "region", "region_type", "sector_group",
+                    "share_of_jurisdiction_emissions_covered")) %>%
     tidyr::extract(
       .data$share_of_jurisdiction_emissions_covered,
       into = c("emissions_coverage", "global_emissions"),
       regex = "(\\d+\\.?\\d*)% of jurisdiction emissions, (\\d+\\.?\\d*)% of global emissions",
       remove = TRUE
     ) %>%
-    left_join(d.metadata %>% select(c("unique_id", "type", "status")) , by = "unique_id") %>%
+    left_join(metadata %>% select(c("unique_id", "type", "status")) , by = "unique_id") %>% # nolint
     dplyr::select(c("unique_id", "region", "region_type", "type", "status", "emissions_coverage")) %>%
     dplyr::mutate(emissions_coverage = as.numeric(.data$emissions_coverage) / 100) %>%
     filter(!(is.na(.data$emissions_coverage)))
 
-  d.price_1_april <- raw.info  %>%
-    left_join(wb.regionMapping, by = "unique_id")  %>%
-    left_join(wb.sectoralMapping, by = "unique_id") %>%
+  priceAprilFirst <- rawInfo  %>%
+    left_join(wbRegionMapping, by = "unique_id")  %>%
+    left_join(wbSectoralMapping, by = "unique_id") %>%
     dplyr::select(c("unique_id", "region", "region_type", "type", "status", "sector_group", "price_on_1_april")) %>%
     tidyr::extract(
       .data$price_on_1_april,
@@ -85,61 +83,124 @@ readWBCarbonPricingDashboard <- function(subtype = "price") {
     select(c("unique_id", "region", "region_type", "type", "status", "sector_group", "price_on_1_april_US")) %>%
     filter(!(is.na(.data$price_on_1_april_US)))
 
-  d.emissionsShare <- raw.emissions %>%
-    dplyr::select(c("name_of_the_initiative", all_of(names(raw.emissions)[suppressWarnings(!is.na(as.numeric(names(raw.emissions))))]))) %>%
-    dplyr::left_join(d.metadata %>%
-                      dplyr::select(c("unique_id", "instrument_name"))
-                    , by = join_by("name_of_the_initiative" == "instrument_name")) %>%
-    left_join(wb.regionMapping, by = "unique_id") %>%
-    left_join(wb.sectoralMapping, by = "unique_id") %>%
-    left_join(d.metadata %>% select(c("unique_id", "type", "status")) , by = "unique_id") %>%
-    dplyr::select(c("unique_id", "region", "region_type", "type", "status", "sector_group", all_of(names(raw.emissions)[suppressWarnings(!is.na(as.numeric(names(raw.emissions))))]))) %>%
-    pivot_longer(-c("unique_id", "region", "region_type", "type", "status", "sector_group"), names_to = "period", values_to = "value") %>%
+  wbRevenue <- rawRevenue %>%
+    dplyr::select(c("instrument_name",
+                    all_of(names(rawRevenue)[suppressWarnings(!is.na(as.numeric(names(rawRevenue))))]))) %>%
+    dplyr::left_join(metadata %>% dplyr::select(c("unique_id", "instrument_name")), by = "instrument_name") %>% # nolint
+    left_join(wbRegionMapping, by = "unique_id") %>%
+    left_join(wbSectoralMapping, by = "unique_id") %>%
+    left_join(metadata %>% select(c("unique_id", "type", "status")), by = "unique_id") %>% # nolint
+    dplyr::select(c("unique_id", "region", "region_type", "type", "status", "sector_group",
+                    all_of(names(rawRevenue)[suppressWarnings(!is.na(as.numeric(names(rawRevenue))))]))) %>%
+    pivot_longer(-c("unique_id", "region", "region_type", "type", "status", "sector_group"),
+                 names_to = "period", values_to = "value") %>%
     filter(!(is.na(.data$value)))
 
-  d.revenue <- raw.revenue %>%
-    dplyr::select(c("instrument_name", all_of(names(raw.revenue)[suppressWarnings(!is.na(as.numeric(names(raw.revenue))))]))) %>%
-    dplyr::left_join(d.metadata %>%
-                      dplyr::select(c("unique_id", "instrument_name"))
-                    , by = "instrument_name") %>%
-    left_join(wb.regionMapping, by = "unique_id") %>%
-    left_join(wb.sectoralMapping, by = "unique_id") %>%
-    left_join(d.metadata %>% select(c("unique_id", "type", "status")) , by = "unique_id") %>%
-    dplyr::select(c("unique_id", "region", "region_type", "type", "status", "sector_group", all_of(names(raw.revenue)[suppressWarnings(!is.na(as.numeric(names(raw.revenue))))]))) %>%
-    pivot_longer(-c("unique_id", "region", "region_type", "type", "status", "sector_group"), names_to = "period", values_to = "value") %>%
+  wbPrice <- rawPrice %>%
+    dplyr::select(c("unique_id", all_of(names(rawPrice)[suppressWarnings(!is.na(as.numeric(names(rawPrice))))]))) %>%
+    left_join(wbRegionMapping, by = "unique_id") %>%
+    left_join(wbSectoralMapping, by = "unique_id") %>%
+    left_join(metadata %>% select(c("unique_id", "type", "status")), by = "unique_id") %>% # nolint
+    dplyr::select(c("unique_id", "region", "region_type", "type", "status", "sector_group",
+                    all_of(names(rawPrice)[suppressWarnings(!is.na(as.numeric(names(rawPrice))))]))) %>%
+    pivot_longer(-c("unique_id", "region", "region_type", "type", "status", "sector_group"),
+                 names_to = "period", values_to = "value") %>%
     filter(!(is.na(.data$value)))
 
-  d.price <- raw.price %>%
-    dplyr::select(c("unique_id", all_of(names(raw.price)[suppressWarnings(!is.na(as.numeric(names(raw.price))))]))) %>%
-    left_join(wb.regionMapping, by = "unique_id") %>%
-    left_join(wb.sectoralMapping, by = "unique_id") %>%
-    left_join(d.metadata %>% select(c("unique_id", "type", "status")) , by = "unique_id") %>%
-    dplyr::select(c("unique_id", "region", "region_type", "type", "status", "sector_group", all_of(names(raw.price)[suppressWarnings(!is.na(as.numeric(names(raw.price))))]))) %>%
-    pivot_longer(-c("unique_id", "region", "region_type", "type", "status", "sector_group"), names_to = "period", values_to = "value") %>%
+  wbEmissionsShare <- rawEmissions %>%
+    dplyr::select(c("name_of_the_initiative",
+                    all_of(names(rawEmissions)[suppressWarnings(!is.na(as.numeric(names(rawEmissions))))]))) %>%
+    dplyr::left_join(metadata %>% dplyr::select(c("unique_id", "instrument_name"))
+                     , by = join_by("name_of_the_initiative" == "instrument_name")) %>%
+    left_join(wbRegionMapping, by = "unique_id") %>%
+    left_join(wbSectoralMapping, by = "unique_id") %>%
+    left_join(metadata %>% select(c("unique_id", "type", "status")), by = "unique_id") %>%  # nolint
+    dplyr::select(c("unique_id", "region", "region_type", "type", "status", "sector_group",
+                    all_of(names(rawEmissions)[suppressWarnings(!is.na(as.numeric(names(rawEmissions))))]))) %>%
+    pivot_longer(-c("unique_id", "region", "region_type", "type", "status", "sector_group"),
+                 names_to = "period", values_to = "value") %>%
     filter(!(is.na(.data$value)))
-  
-  switch(subtype,
-    "price" = { data <- d.price },
-    "revenue" = { data <- d.revenue },
-    "emissions_share" = { data <- d.emissionsShare },
-    "emissions" = { data <- d.emissionsShare },
-    "price_1_april" = { data <- d.price_1_april })
 
-  EU27 <- c("Austria" = "AUT", "Belgium" = "BEL", "Bulgaria" = "BGR", "Croatia" = "HRV", "Cyprus" = "CYP", "Czech Republic" = "CZE", "Denmark" = "DNK", "Estonia" = "EST", "Finland" = "FIN", "France" = "FRA", "Germany" = "DEU", "Greece" = "GRC", "Hungary" = "HUN", "Ireland" = "IRL", "Italy" = "ITA", "Latvia" = "LVA", "Lithuania" = "LTU", "Luxembourg" = "LUX", "Malta" = "MLT", "Netherlands" = "NLD", "Poland" = "POL", "Portugal" = "PRT", "Romania" = "ROU", "Slovakia" = "SVK", "Slovenia" = "SVN", "Spain" = "ESP", "Sweden" = "SWE")
-  EU_ETS <- c(EU27, "Iceland" =	"ISL", "Liechtenstein" = "LIE", "Norway" = "NOR")
-
-  data <- bind_rows(
-    data %>% dplyr::filter(.data$region_type == "country"),
-    data %>%
-      dplyr::filter(.data$unique_id == "ETS_EU") %>%
-      dplyr::select(-c("region")) %>%
-      tidyr::crossing(region = EU_ETS) %>%
-      dplyr::mutate(region_type = "country") %>%
-      select(all_of(names(data)))
+  # raw emissions data to calculate covered emissions per country
+  histEmiRaw <- quitte::as.quitte(madrat::readSource("EDGARghg")) %>%
+    select(c("region", "period", "variable", "pollutant", "value"))
+  histEmiSectorRaw <- histEmiRaw %>%
+    dplyr::bind_rows( # repeat last year for missing years
+      histEmiRaw %>% filter(.data$period == 2023) %>% mutate(period = 2024),
+      histEmiRaw %>% filter(.data$period == 2023) %>% mutate(period = 2025)
     ) %>%
+    dplyr::group_by(.data$region, .data$period, .data$variable) %>%
+    dplyr::summarize(value = sum(.data$value, na.rm = TRUE), .groups = "drop")
+  histEmiGlo <- histEmiSectorRaw %>%
+    dplyr::group_by(.data$period) %>%
+    dplyr::summarize(value = sum(.data$value, na.rm = TRUE), .groups = "drop") %>%
+    mutate(period = as.character(.data$period))
+  wbEmissionsCovered <- wbEmissionsShare %>%
+    left_join(histEmiGlo, by = "period", suffix = c("_share", "_emiGLO")) %>%
+    mutate(value = .data$value_share * .data$value_emiGLO) %>%
+    select(-c("value_share", "value_emiGLO"))
+
+  switch(subtype,
+         "price" = { dd <- wbPrice }, # nolint
+         "revenue" = { dd <- wbRevenue }, # nolint
+         "wbCoverage" = { dd <- wbCoverage }, # nolint
+         "priceAprilFirst" = { dd <- priceAprilFirst }, # nolint
+         "emissions_coverage" = { dd <- wbEmissionsCovered }) # nolint
+
+  #expand EU ETS data to all EU countries
+  EU27 <- c("AUT", "BEL", "BGR", "HRV", "CYP", "CZE", "DNK", "EST", "FIN", "FRA", "DEU", "GRC", "HUN", "IRL", "ITA", # nolint
+            "LVA", "LTU", "LUX", "MLT", "NLD", "POL", "PRT", "ROU", "SVK", "SVN", "ESP", "SWE")
+  EU_ETS <- c(EU27, "ISL", "LIE", "NOR") # nolint
+
+  if (subtype %in% c("price", "revenue", "priceAprilFirst")) {
+    # EU ETS price = country ETS price
+    data <- bind_rows(
+      dd %>% dplyr::filter(.data$region_type == "country"),
+      dd %>%
+        dplyr::filter(.data$unique_id == "ETS_EU") %>%
+        dplyr::select(-c("region")) %>%
+        tidyr::crossing(region = EU_ETS) %>%
+        dplyr::mutate(region_type = "country") %>%
+        select(all_of(names(dd)))
+    )
+  } else if (subtype %in% c("emissions_covered")) {
+    # country ETS coverage proportional to country bulk emissions size
+    bulk <- c("Industrial Combustion", "Power Industry", "Processes", "Fuel Exploitation")
+    emiBulk <- histEmiSectorRaw %>%
+      dplyr::filter(.data$variable %in% bulk) %>%
+      dplyr::group_by(.data$region, .data$period) %>%
+      dplyr::summarize(value = sum(.data$value, na.rm = TRUE), .groups = "drop") %>%
+      dplyr::mutate(period = as.character(.data$period))
+    emiBulkShare <- wbEmissionsCovered %>%
+      filter(.data$unique_id == "ETS_EU") %>%
+      select(dplyr::where(~length(unique(.x)) > 1)) %>%
+      left_join(emiBulk %>% filter(.data$region %in% EU_ETS) %>% group_by(.data$period) %>% # nolint
+                  summarize(value = sum(.data$value, na.rm = TRUE), .groups = "drop"),
+                by = "period", suffix = c("_emi", "_wb")) %>%
+      mutate(share = .data$value_emi / .data$value_wb) %>%
+      select(c("period", "share"))
+    emiCalcBulk <- emiBulk %>%
+      filter(.data$region %in% EU_ETS) %>%
+      left_join(emiBulkShare, by = c("period")) %>%
+      mutate(value = coalesce(.data$value * .data$share, 0)) %>%
+      select(-c("share"))
+    data <- bind_rows(
+      dd %>% dplyr::filter(.data$region_type == "country"),
+      emiCalcBulk %>%
+        mutate(
+          unique_id = "ETS_EU",
+          region_type = "country",
+          type = "ets",
+          status = "implemented",
+          sector_group = "bulk"
+        ) %>%
+        select(all_of(names(dd)))
+    )
+  }
+
+  out <- data %>%
     dplyr::select(-c("unique_id", "region_type")) %>%
     as.magpie(spatial = "region", temporal = "period", datacol = "value")
 
-  # Warning emissions and emissions shares require the convert function code to provide the correct output
-  return(data)
+  return(out)
 }
