4a18f1bd70a58745a5b60cc46e48be4ed3a456b5
Renato-Rodrigues <renato.rodrigues@pik-potsdam.de>
2025-08-11 14:42:42 +0200

    add global economy data read functions

--- R/convertGlobalEconomyData.R
+++ R/convertGlobalEconomyData.R
@@ -1,10 +1,10 @@
-#' Convert Read World Bank Carbon Pricing Dashboard data
+#' Convert Global economy data indicators
+#' aggregation of over 500 indicators for over 200 countries from 1960 - 2024.
+#' sources: central banks, national statistical institutes, and multiple international organizations.
 #'
 #' @md
 #' @param x A [`magpie`][magclass::magclass] object returned from
 #'          [`readWBCarbonPricingDashboard()`].
-#'
-#' @param subtype character, type of data: price, priceAprilFirst, wbCoverage, revenue, emissions_covered
 #'
 #' @return A [`magpie`][magclass::magclass] object.
 #'
@@ -13,12 +13,12 @@
 #' @importFrom madrat toolCountryFill toolFillYears
 #' @importFrom magclass getYears magpiesort
 #'
-convertWBCarbonPricingDashboard <- function(x, subtype) {
+convertGlobalEconomyData <- function(x) {
 
   x[is.na(x)] <- 0 # setting NA values to zero
   x <- madrat::toolFillYears(x, sort(magclass::getYears(x, as.integer = TRUE)))
   x <- madrat::toolCountryFill(x, fill = 0, verbosity = 2)
   x <- magpiesort(x)
-  
+
   return(x)
 }
---
+++ R/readGlobalEconomyData.R
@@ -0,0 +1,37 @@
+#' Global economy data indicators
+#' aggregation of over 500 indicators for over 200 countries from 1960 - 2024.
+#' sources: central banks, national statistical institutes, and multiple international organizations.
+#'
+#' @returns MagPIE object with global economy data indicators
+#'
+#' @author Renato Rodrigues
+#'
+#' @importFrom tidyr pivot_longer
+#' @importFrom dplyr mutate select rename filter
+#'
+readGlobalEconomyData <- function() {
+
+  files <- list.files(path = "./", pattern = "\\.csv$", full.names = TRUE)
+
+  data <- do.call(
+    rbind,
+    lapply(files, function(file) {
+      df <- read.csv(file, stringsAsFactors = FALSE)
+      df <- df %>%
+        pivot_longer(cols = setdiff(names(df), c("Country", "Code", "ContinentCode", "Year")),
+                     names_to = "driver", values_to = "value") %>%
+        mutate(type = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(file)),
+               driver = gsub(".", "_", .data$driver, fixed = TRUE)) %>%
+        select("Code", "Year", "type", "driver", "value") %>%
+        rename(region = "Code", period = "Year")
+    })
+  ) %>%
+    mutate(region = ifelse(.data$region == "CRC", "CRI", # fixing Costa Rica iso3 code
+                         ifelse(.data$region == "PS", "PSE", .data$region))) %>% # fixing Palestine iso3 code
+    filter(!(.data$region %in% c("XC"))) # removing euro area data
+
+  out <- data %>%
+    as.magpie(spatial = "region", temporal = "period", datacol = "value")
+
+  return(out)
+}
